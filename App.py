e08b29bdab21a46e6bfe80d6384cc69b0e4ad33f
CimiChin <151364311+CimiChin@users.noreply.github.com>
2025-05-22 08:43:10 +0700

    Update App.py

--- App.py
+++ App.py
@@ -4,8 +4,4 @@ from PIL import Image
 st.title("Naruto vs Sasuke")
 st.write("Pertarungan legendaris antara dua ninja hebat!")
 
-# Tampilkan gambar
-image = Image.open("images/naruto_vs_sasuke.jpg")
-st.image(image, caption="Naruto vs Sasuke", use_column_width=True)
-
-st.markdown("Siapa jagoan kamu? 🤜🤛")
+st.image("https://id.pinterest.com/pin/591871576023586470/", caption=" ikeeeeh", use_column_width=True
