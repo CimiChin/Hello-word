d108c2b0e9562f6dde2c3c12056d79d0da9c860f
CimiChin <151364311+CimiChin@users.noreply.github.com>
2025-05-22 08:46:08 +0700

    Update App.py

--- App.py
+++ App.py
@@ -4,4 +4,4 @@ from PIL import Image
 st.title("Naruto vs Sasuke")
 st.write("Pertarungan legendaris antara dua ninja hebat!")
 
-st.image("https://www.google.com/imgres?q=sasuke%20vs%20naruto&imgurl=https%3A%2F%2Fthumb.viva.id%2Fvivabanyuwangi%2F665x374%2F2024%2F11%2F09%2F672f05fb7842f-anime-naruto_banyuwangi.jpg&imgrefurl=https%3A%2F%2Fbanyuwangi.viva.co.id%2Fgaya-hidup%2F10234-naruto-vs-sasuke-persaingan-abadi-dan-perkembangan-kekuatan-dalam-dunia-ninja&docid=QRYatP6o7bNJuM&tbnid=Tj89MX0T_YXymM&vet=12ahUKEwj6kIT3-LWNAxW8SmwGHSALJkkQM3oECBsQAA..i&w=665&h=374&hcb=2&ved=2ahUKEwj6kIT3-LWNAxW8SmwGHSALJkkQM3oECBsQAA", caption=" ikeeeeh", use_column_width=True
+st.image("https://banyuwangi.viva.co.id/foto/6608-naruto-vs-sasuke-dua-sisi-mata-uang-kekuatan", caption=" ikeeeeh", use_column_width=True
