67f8ce270fff72517bdc639320f658b192b2d75b
CimiChin <151364311+CimiChin@users.noreply.github.com>
2025-05-22 09:09:38 +0700

    Update App.py

--- App.py
+++ App.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
-st.title("China")
-st.write("Pukulan orang china")
-st.image ("https://i.pinimg.com/736x/cc/f1/d6/ccf1d67163647f6d66219ef5e5fabff5.jpg", caption=" chao ni ma!", use_column_width=True)
+st.set_page_config(page_title="uwawawawawa", layout="centered")
+
+st.title("helllo")
+st.write("hello gs")
+
