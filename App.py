c4873c3f4c1e30885400a4fa714bde34cc6a5e16
CimiChin <151364311+CimiChin@users.noreply.github.com>
2025-05-22 08:47:45 +0700

    Update App.py

--- App.py
+++ App.py
@@ -1,4 +1,5 @@
 import streamlit as st
 
-st.title("Hello, World!")
-st.write("Ini adalah aplikasi Streamlit paling sederhana.")
+st.title("STD!")
+st.write("MATARAM IS RED MATARAM IS BLUE")
+st.image ("https://i.pinimg.com/736x/02/f5/3c/02f53ccf7c42284b1815689fc9e11697.jpg", caption=" JAWA!", use_column_width=True)
