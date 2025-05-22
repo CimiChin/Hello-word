488b0e80feea6a83ffec8824d652df740138d89e
CimiChin <151364311+CimiChin@users.noreply.github.com>
2025-05-22 09:06:57 +0700

    Update 2_apasih.py

--- pages/2_apasih.py
+++ pages/2_apasih.py
@@ -1,1 +1,10 @@
+import streamlit as st
+import pandas as pd
+import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, accuracy_score
+
+st.set_page_config(page_title="Industry")
+st.title("Industry")
+st.header("Industry")
 
