6e97a54b5f2b927e56b0bb24a7cb42ae002ed501
CimiChin <151364311+CimiChin@users.noreply.github.com>
2025-05-22 08:38:51 +0700

    Update App.py

--- App.py
+++ App.py
@@ -1,4 +1,11 @@
 import streamlit as st
+from PIL import Image
 
-st.title("Hello, World!")
-st.write("Ini adalah aplikasi Streamlit paling sederhana.")
+st.title("Naruto vs Sasuke")
+st.write("Pertarungan legendaris antara dua ninja hebat!")
+
+# Tampilkan gambar
+image = Image.open("images/naruto_vs_sasuke.jpg")
+st.image(image, caption="Naruto vs Sasuke", use_column_width=True)
+
+st.markdown("Siapa jagoan kamu? 🤜🤛")
